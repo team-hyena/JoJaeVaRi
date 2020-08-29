36e0247bd07ef6f76d76127911f4d6a3c1371883
hyena-coder <hyena.coder@gmail.com>
2020-08-29 16:53:24 +0900

    인피니티 스크롤을 위한 2가지 방법
    - /club/items1?start=?&count=? 방법, 시작 id번호와 개수를 사용해서 가져오는 방법입니다.
    - /club/items2?paginate=? 방법, 정해진 paginate 만큼 가져오는 방법으로 1, 2, ... 와 같이 번호를 넣어 가져오는 방법입니다.

--- club/urls.py
+++ club/urls.py
@@ -5,6 +5,8 @@ app_name = 'club'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('items1/', views.inf_scroll1, name='items1'),
+    path('items2/', views.inf_scroll2, name='items2'),
     path('make/', views.make, name='make'),
     path('<int:club_id>/', views.detail, name='detail'),
     path('generator/', views.fake_generator, name='generator')
